0898c309de053cc6393036dfb61f35d5777b7a2d
Viihctor <Viihctor@users.noreply.github.com>
2025-03-11 16:49:04 -0300

    Codigo do Projeto

---
+++ Code/src/pokemontrainercard/loginInBetween.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemontrainercard;
+
+/**
+ *
+ * @author 920415
+ */
+public class loginInBetween {
+    private static String LoginId;
+
+    public static String getLoginId() {
+        return LoginId;
+    }
+
+    public static void setLoginId(String value) {
+        LoginId = value;
+    }
+}
